0bc3f443a0a0bd8e521bcddeb02c914cdcf90333
ie4 <kei@ie4.me>
2015-05-05 10:54:42 +0000

    Scraper library update

--- lib/billboard_chart/billboard_chart_scraper.rb
+++ lib/billboard_chart/billboard_chart_scraper.rb
@@ -14,36 +14,36 @@ class BillboardChartScraper
   # =============== Scrape Methods ============= #
 
   def get_artists
-    @scraped_data.css("p.chart_info a").collect {|artist| artist.text}
+    @scraped_data.css(".chart-row h3").collect {|artist| artist.text.strip!}
   end
 
   def get_singles
-    @scraped_data.css("h1").collect {|single| single.text}[1..10]
+    @scraped_data.css(".chart-row h2").collect {|single| single.text.strip!}
   end
 
   def merge_artists_and_singles
-    Hash[get_artists.zip(get_singles)]
+    get_artists.zip(get_singles)
   end
 
   # =============== Display Methods ============= #
-  
+
   def top_ten
-    puts "\n"
-    puts "✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭".yellow
-    puts "\n"
+    print_line
     puts "      ♫  Top 10 Billboard Hot 100 Singles Chart: ♫    "
     puts "                  (#{DateTime.now.strftime('%m/%d/%Y')})             ".red
     puts "\n"
-    i = 0
-    while i < 10
-      merge_artists_and_singles.each do |artist, single|
-        puts "#{i+1}. #{artist} - #{single}"
-        i += 1
-      end
-        puts "\n"
-        puts "✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭".yellow
-        puts "\n"
+    rank = 0
+    merge_artists_and_singles.each do |artist, single|
+      rank += 1
+      puts "#{rank}. #{artist} - #{single}"
+      print_line if (rank % 10 == 0)
     end
+  end
+
+  def print_line
+    puts "\n"
+    puts "✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭ ✭".yellow
+    puts "\n"
   end
 
 end
